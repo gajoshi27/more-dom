7a6c8465fb29f61bfb2ca74064fe817a71cbc47c
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2023-07-31 17:31:47 -0400

    Add @TODOS for JS

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+/*  @TODO - Add references to the elements we want to use  */
+
+
+
+
+/*  @TODO - Create a Function for our changeButton to change first Pokemon  */
+
+
+
+
+/*  @TODO - Create a Function for our addButton to Add New Pokemon  */
+
+
+
+
+
+
+/*   @TODO - Create a Function for our deleteButton to Delete the First Pokemon   */
+
+
+
 
