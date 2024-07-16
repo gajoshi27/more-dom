588982e006c592d705dddb078972ae64dd274913
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2024-07-16 18:26:43 -0400

    Finalized @TODOs

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 /*  @TODO - Add references to the elements we want to use  */
 
-
-
+/*  @TODO - Create an array of Pokemon to select from  */
 
 /*  @TODO - Create a Function for our changeButton to change first Pokemon  */
 
-
-
-
 /*  @TODO - Create a Function for our addButton to Add New Pokemon  */
 
-
-
-
-
-
 /*   @TODO - Create a Function for our deleteButton to Delete the First Pokemon   */
-
-
-
-
